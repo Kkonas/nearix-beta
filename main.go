73fe02a8b23eced005fdf1768ec50548c5c90696
joker-ware <steew0x8@protonmail.com>
2020-02-20 22:29:29 +0100

    Added support for languages

--- main.go
+++ main.go
@@ -29,26 +29,39 @@ func writeFile(path string, buffer []byte){
 	err := ioutil.WriteFile(path, buffer, 0200)
 	logErr(err)
 }
-func readYaml(path string, confStruct *Config){
+func readConfigYaml(path string, structPointer *Config){
 	reader := readFile(path)
-	err := yaml.Unmarshal(reader, confStruct)
+	err := yaml.Unmarshal(reader, structPointer)
 	logErr(err)
 }
-func writeYaml(path string,confStruct *Config){
+func readLangYaml(path string, structPointer *LangConfig){
+	reader := readFile(path)
+	err := yaml.Unmarshal(reader, structPointer)
+	logErr(err)
+}
+
+func writeConfigYaml(path string,confStruct *Config){
 	output, err := yaml.Marshal(confStruct)
 	logErr(err)
-	writeFile(path, output)
+writeFile(path, output)
 }
-func initCheck(confStruct *Config){
-	fmt.Printf("Do you wanna restore last session? Y/n:\n")
-	response := strings.ToLower(readStdin())
-	if response == "y"{
-		readYaml(configFile, confStruct)
-	}else if response == "n"{
-		return
-	}else{
-		os.Exit(3)
+func writeLangYaml(path string,langStruct *LangConfig){
+	output, err := yaml.Marshal(langStruct)
+	logErr(err)
+writeFile(path, output)
+}
+
+func initCheck(confStruct *Config,langStruct *LangConfig){
+	readConfigYaml("config/config.yaml",confStruct)
+	readLangYaml("config/languages.yaml",langStruct)
+	if confStruct.Token == ""{
+		fmt.Printf(Lang(confStruct,langStruct,1,"emptytoken"))
+		response := readStdin()
+		confStruct.Token = response
 	}
+}
+func Lang(confStruct *Config,langStruct *LangConfig, id int,message string) string{
+	return langStruct.Languages[confStruct.Constants.Language][id][message]
 }
 func logErr(err error){
 	if err != nil{
@@ -60,15 +73,22 @@ func readStdin() string{
 	raw ,_ := reader.ReadString('\n')
 	return strings.Replace(raw,"\n","",1)
 }
-func messageCreate(session *discordgo.Session,message *discordgo.MessageCreate){
+/*func messageCreate(session *discordgo.Session,message *discordgo.MessageCreate){
 	if message.Author.ID != conf.Constants.PokeCordID{
 	return
 	}else{
 		embeds := message.Embeds
+		for _, embed := range embeds{
+			spawnUrl := embed.Image.URL
+
+		}
 	}
-}
+}*/
 // ENDOF function declaration section
 // Begin structs
+type LangConfig struct{
+	Languages	map[string][]map[string]string
+}
 type Config struct{
 	Token		string
 	Constants	Const
@@ -77,6 +97,7 @@ type Config struct{
 }
 type Const struct{
 	PokeCordID	string
+	Language	string
 }
 type State struct{
 	Guilds		[]Guild
@@ -95,9 +116,10 @@ type Channel struct{
 
 // Begin main
 func main(){
+	var lang LangConfig
 	var conf Config
-	initCheck(&conf)
-	fmt.Printf("Token: %s, Version: %s, Guilds: %s \n",conf.Token,conf.Version, conf.Session.Guilds)
+	initCheck(&conf,&lang)
+	fmt.Printf("Token: %s, Version: %s, Guilds: %s ,ID: %s\n",conf.Token,conf.Version, conf.Session.Guilds, conf.Constants.PokeCordID)
 	client, err := discordgo.New(conf.Token)
 	logErr(err)
 	err = client.Open()
@@ -110,5 +132,5 @@ func main(){
 			conf.Session.Guilds[index].Channels = append(conf.Session.Guilds[index].Channels, appendChannel)
 		}
 	}
-	writeYaml(configFile,&conf)
+	writeConfigYaml(configFile,&conf)
 }
