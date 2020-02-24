8a65a6f6bc015a16911bda9fa6c299e12d519aa4
Bilibox <1800580+Bilibox@users.noreply.github.com>
2020-02-23 16:48:42 -0800

    Update LINT
    Updates code to be compliant with LINT

--- detect.go
+++ detect.go
@@ -1,40 +1,46 @@
 // This file will receive a Pokemon Image and will return the appropriate pokemon.
-// This will get called from main.go
+// This will get called from jokercord.go
 
 package main
 
 import (
-	"net/http"
-	"gopkg.in/yaml.v2"
-	"os"
-	"io/ioutil"
 	"crypto/md5"
 	"encoding/hex"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
+
 // BEGIN structs
 // ENDOF structs
+
 // BEGIN function definition
-func receive(url string) string{
-	pokemons:= make(map[string]string)
-	err := Download("images/template.jpg",url)
+// receive grabs url of Pokemon picture
+func receive(url string) string {
+	pokemons := make(map[string]string)
+	err := Download("images/template.jpg", url)
 	logErr(err)
 	hash := Hash("images/template.jpg")
 	hashHex := hex.EncodeToString(hash)
 	readPokemonList(pokemons)
 	pokemonName := Compare(hashHex, pokemons)
-	return(pokemonName)
+	return (pokemonName)
 }
-func Download(path string, url string) error{
+
+// Download grabs Pokemon Picture from recieve url
+func Download(path string, url string) error {
 	response, err := http.Get(url)
 	logErr(err)
 	var output *os.File
 
-	if _, err := os.Stat(path); os.IsNotExist(err){
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		logErr(err)
 		output, err = os.Create(path)
 		logErr(err)
-	}else{
+	} else {
 		err = os.Remove(path)
 		logErr(err)
 		output, err = os.Create(path)
@@ -46,22 +52,28 @@ func Download(path string, url string) error{
 	defer output.Close()
 	return err
 }
-func readPokemonList(pokemonStruct map[string]string){
+
+// readPokemonList reads hash list
+func readPokemonList(pokemonStruct map[string]string) {
 	reader, err := ioutil.ReadFile("config/hashes.yaml")
 	logErr(err)
 	yaml.Unmarshal(reader, pokemonStruct)
 }
-func Compare(hash string, pokemonStruct map[string]string) string{
+
+// Compare checks hash to hash list
+func Compare(hash string, pokemonStruct map[string]string) string {
 	var name string
-	for pokemon,pokemonHash := range pokemonStruct{
-		if pokemonHash == hash{
-		name = pokemon
+	for pokemon, pokemonHash := range pokemonStruct {
+		if pokemonHash == hash {
+			name = pokemon
 		}
 	}
 	return name
 
 }
-func Hash(path string) []byte{
+
+// Hash grabs value from Download
+func Hash(path string) []byte {
 	output, err := ioutil.ReadFile(path)
 	logErr(err)
 	hash := md5.New()
@@ -70,7 +82,7 @@ func Hash(path string) []byte{
 	hexEncoded := make([]byte, hex.EncodedLen(len(hashSum)))
 	hex.Encode(hexEncoded, hashSum)
 
-
 	return hexEncoded
 }
+
 // ENDOF function definition
--- jokercord.go
+++ jokercord.go
@@ -1,62 +1,66 @@
 /* Welcome to the discord multi spam bot
- Author: SteeW (a.k.a joker-ware)
- Following code may or may not be commented.
- "Abandon all hope ye who enter here" */
+Author: SteeW (a.k.a joker-ware)
+Following code may or may not be commented.
+"Abandon all hope ye who enter here" */
 
 package main
+
 import (
-	"fmt"
 	"bufio"
-	"strings"
+	"fmt"
 	"io/ioutil"
-	"os/signal"
-	"syscall"
-	"os"
 	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
 	"github.com/bwmarrin/discordgo"
 	"gopkg.in/yaml.v2"
 )
+
 // Begin CONSTS
 
 const configFile = "config/config.yaml"
 
 // ENDOF CONSTS
+
 // Begin function declaration section
-func readFile(path string) []byte{
+func readFile(path string) []byte {
 	file, err := ioutil.ReadFile(path)
 	logErr(err)
 	return file
 }
-func writeFile(path string, buffer []byte){
+func writeFile(path string, buffer []byte) {
 	err := ioutil.WriteFile(path, buffer, 0200)
 	logErr(err)
 }
-func readConfigYaml(path string, structPointer *Config){
+func readConfigYaml(path string, structPointer *Config) {
 	reader := readFile(path)
 	err := yaml.Unmarshal(reader, structPointer)
 	logErr(err)
 }
-func readLangYaml(path string, structPointer *LangConfig){
+func readLangYaml(path string, structPointer *LangConfig) {
 	reader := readFile(path)
 	err := yaml.Unmarshal(reader, structPointer)
 	logErr(err)
 }
 
-func writeConfigYaml(path string,confStruct *Config){
+func writeConfigYaml(path string, confStruct *Config) {
 	output, err := yaml.Marshal(confStruct)
 	logErr(err)
-writeFile(path, output)
+	writeFile(path, output)
 }
-func writeLangYaml(path string,langStruct *LangConfig){
+func writeLangYaml(path string, langStruct *LangConfig) {
 	output, err := yaml.Marshal(langStruct)
 	logErr(err)
-writeFile(path, output)
+	writeFile(path, output)
 }
 
-func initCheck(confStruct *Config,langStruct *LangConfig){
-	readConfigYaml("config/config.yaml",confStruct)
-	readLangYaml("config/languages.yaml",langStruct)
-	if confStruct.Token == ""{
+func initCheck(confStruct *Config, langStruct *LangConfig) {
+	readConfigYaml("config/config.yaml", confStruct)
+	readLangYaml("config/languages.yaml", langStruct)
+	if confStruct.Token == "" {
 		fmt.Printf(Lang("emptytoken"))
 		fmt.Printf(Lang("tokenprompt"))
 		response := readStdin()
@@ -64,118 +68,136 @@ func initCheck(confStruct *Config,langStruct *LangConfig){
 		updateConfigYaml()
 	}
 }
-func Lang(message string) string{
+
+// Lang grabs user lanuage for translations
+func Lang(message string) string {
 	return lang.Languages[conf.Constants.Language][message]
 }
-func logErr(err error){
-	if err != nil{
-		readConfigYaml("config/config.yaml",&conf)
-		readLangYaml("config/languages.yaml",&lang)
+
+// LogErr is used for error logging (DUH)
+func logErr(err error) {
+	if err != nil {
+		readConfigYaml("config/config.yaml", &conf)
+		readLangYaml("config/languages.yaml", &lang)
 		log.Print(lang.Languages[conf.Constants.Language]["error"])
 	}
 }
-func readStdin() string{
+
+// readStdin reads user input data
+func readStdin() string {
 	reader := bufio.NewReader(os.Stdin)
-	raw ,_ := reader.ReadString('\n')
-	return strings.Replace(raw,"\n","",1)
+	raw, _ := reader.ReadString('\n')
+	return strings.Replace(raw, "\n", "", 1)
 }
-func messageCreate(session *discordgo.Session,message *discordgo.MessageCreate){
-	if message.Author.ID != conf.Constants.PokeCordID{
-	return
-	}else{
-		if message.Embeds == nil{
-			return
-		}else{
-		embeds := message.Embeds
-		for _, embed := range embeds{
-			if (embed.Image == nil){
-				return
-			}else{
-				spawnUrl := embed.Image.URL
-				session.ChannelMessageSend(message.ChannelID,"p!catch "+receive(spawnUrl))
+
+// messageCreate creates message for Catching
+func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
+	if message.Author.ID == conf.Constants.PokeCordID {
+		if message.Embeds != nil {
+			embeds := message.Embeds
+			for _, embed := range embeds {
+				if embed.Image != nil {
+					spawnURL := embed.Image.URL
+					session.ChannelMessageSend(message.ChannelID, "p!catch "+receive(spawnURL))
 				}
 			}
 		}
 	}
 }
-func refresh(client *discordgo.Session){
-	if len(conf.Session.Guilds) == 0{
-	for index, guild := range client.State.Guilds{
-		appendGuild := Guild{guild.ID,guild.Name,nil}
-		conf.Session.Guilds = append(conf.Session.Guilds,appendGuild)
-		for _, channel := range guild.Channels{
-			appendChannel := Channel{channel.ID,channel.Name,false}
-			conf.Session.Guilds[index].Channels = append(conf.Session.Guilds[index].Channels, appendChannel)
+
+// refresh updates user guild info
+func refresh(client *discordgo.Session) {
+	if len(conf.Session.Guilds) == 0 {
+		for index, guild := range client.State.Guilds {
+			appendGuild := Guild{guild.ID, guild.Name, nil}
+			conf.Session.Guilds = append(conf.Session.Guilds, appendGuild)
+			for _, channel := range guild.Channels {
+				appendChannel := Channel{channel.ID, channel.Name, false}
+				conf.Session.Guilds[index].Channels = append(conf.Session.Guilds[index].Channels, appendChannel)
+			}
+		}
+		if len(conf.Session.Guilds) != 0 {
+			updateConfigYaml()
 		}
 	}
-	if len(conf.Session.Guilds) == 0{
-		return
-	}else{
-	updateConfigYaml()
-	}
-	}
 }
-func updateConfigYaml(){
-	writeConfigYaml("config/config.yaml",&conf)
+func updateConfigYaml() {
+	writeConfigYaml("config/config.yaml", &conf)
 }
-func init(){
-	initCheck(&conf,&lang)
-	readConfigYaml(configFile,&conf)
+func init() {
+	initCheck(&conf, &lang)
+	readConfigYaml(configFile, &conf)
 	client, err := discordgo.New(conf.Token)
 	logErr(err)
 	client.AddHandler(messageCreate)
-	if conf.Constants.First == true{
+	if conf.Constants.First == true {
 		fmt.Printf("Please choose your language code. Available languages are: ")
-		for languageName,_ := range lang.Languages{
-			fmt.Printf(languageName+"|")
+		for languageName := range lang.Languages {
+			fmt.Printf(languageName + "|")
 		}
 		fmt.Printf(" :")
 		selectedLang := readStdin()
-		if lang.Languages[selectedLang] != nil || len(lang.Languages[selectedLang]) != 0{
+		if lang.Languages[selectedLang] != nil || len(lang.Languages[selectedLang]) != 0 {
 			conf.Constants.Language = selectedLang
 			conf.Constants.First = false
 			updateConfigYaml()
-		}else{
+		} else {
 			fmt.Printf("Chosen language is not valid.")
 			log.Fatal("Language error.")
 		}
 		err = client.Open()
-		if err != nil{
-		log.Fatal(lang.Languages[conf.Constants.Language]["tokenerror"])}
+		if err != nil {
+			log.Fatal(lang.Languages[conf.Constants.Language]["tokenerror"])
+		}
 		refresh(client)
-	}else{
+	} else {
 		err = client.Open()
 	}
 }
+
 // ENDOF function declaration section
+
 // Begin structs
-type LangConfig struct{
-	Languages	map[string]map[string]string
+
+// LangConfig hold the Language that should be used
+type LangConfig struct {
+	Languages map[string]map[string]string
 }
-type Config struct{
-	Token		string
-	Constants	Const
-	Version		string
-	Session		State
+
+// Config holds all information required for this program
+type Config struct {
+	Token     string
+	Constants Const
+	Version   string
+	Session   State
 }
-type Const struct{
-	PokeCordID	string
-	Language	string
-	First		bool
+
+// Const holds information required to be constant
+type Const struct {
+	PokeCordID string
+	Language   string
+	First      bool
 }
-type State struct{
-	Guilds		[]Guild
+
+// State holds each Guild a user has access to
+type State struct {
+	Guilds []Guild
 }
-type Guild struct{
-	Id		string
-	Name		string
-	Channels	[]Channel
+
+// Guild holds required Guild information
+type Guild struct {
+	ID       string
+	Name     string
+	Channels []Channel
 }
-type Channel struct{
-	Id		string
-	Name		string
-	Enabled		bool
+
+// Channel holds channel information of each Guild
+type Channel struct {
+	ID      string
+	Name    string
+	Enabled bool
 }
+
 // ENDOF structs
 
 // Begin main
@@ -183,9 +205,10 @@ type Channel struct{
 var lang LangConfig
 var conf Config
 var client discordgo.Session
+
 // ENDOF GLOBAL values
-func main(){
-	fmt.Printf("Token: %s, Version: %s, ID: %s\n",conf.Token,conf.Version, conf.Constants.PokeCordID)
+func main() {
+	fmt.Printf("Token: %s, Version: %s, ID: %s\n", conf.Token, conf.Version, conf.Constants.PokeCordID)
 
 	fmt.Println(lang.Languages[conf.Constants.Language]["running"])
 	sc := make(chan os.Signal, 1)
